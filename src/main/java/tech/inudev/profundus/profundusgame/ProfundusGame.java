ed7ef5dc3779dd0abd6270ef2101317f97e5aa2a
Riku Ueda <riku.ueda@outlook.jp>
2022-07-27 15:59:11 +0900

    Initial commit

---
+++ src/main/java/tech/inudev/profundus/profundusgame/ProfundusGame.java
@@ -0,0 +1,17 @@
+package tech.inudev.profundus.profundusgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProfundusGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
